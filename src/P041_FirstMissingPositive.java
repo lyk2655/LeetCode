a465754f345d1ce072af374394e9746c271f3a2b
lyk2655 <lyk2655@126.com>
2019-09-01 00:05:59 +0800

    combination sum

--- src/P041_FirstMissingPositive.java
+++ src/P041_FirstMissingPositive.java
@@ -11,7 +11,10 @@ public class P041_FirstMissingPositive {
             return 1;
         }
         for(int i = 0; i < nums.length; i++) {
-            if(nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[nums[i] - 1]) {
+            if(nums[i] == i+1) {
+                continue;
+            }
+            while(nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[nums[i] - 1]) {
                 int temp = nums[i];
                 nums[i] = nums[nums[i] -1];
                 nums[temp-1] = temp;
