f046fae2926c736e37cfcdacc566ddce6af8f952
lyk2655 <lyk2655@126.com>
2019-09-05 23:56:55 +0800

    combination sum

---
+++ src/P042_TrappingRainWater.java
@@ -0,0 +1,13 @@
+public class P042_TrappingRainWater {
+
+    public static void main(String[] args) {
+        P042_TrappingRainWater trappingRainWater = new P042_TrappingRainWater();
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trappingRainWater.trap(height));
+
+    }
+
+    public int trap(int[] height) {
+        return 0;
+    }
+}
